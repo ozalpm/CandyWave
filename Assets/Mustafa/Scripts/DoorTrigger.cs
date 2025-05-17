49d698cb46bfd7e02eae02eacc29e2ee02d34e29
Alper Ozkan <alprozkan@gmail.com>
2025-05-18 00:02:41 +0300

    Home implementation

---
+++ Assets/Mustafa/Scripts/DoorTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public DoorController door;
+
+    private bool playerInRange = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            door.ToggleDoor();
+        }
+    }
+}
