0c0a86867554fb0c92b70edd5afb7b55be5f9bda
ozalpm <ozalpm1@gmail.com>
2025-05-17 20:30:34 +0300

    da

---
+++ Assets/Mustafa/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Hertzole.GoldPlayer;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GoldPlayerGraphics graphics;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
